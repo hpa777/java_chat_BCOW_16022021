8c49922bafbb70a4ef9c74c6732b0eb978c84bdf
Pavel A. Khanin <pkhanyn@gmail.com>
2021-02-27 15:27:41 +0300

    set socket timeout

--- server/src/main/java/server/ClientHandler.java
+++ server/src/main/java/server/ClientHandler.java
@@ -7,6 +7,7 @@ import java.io.DataInputStream;
 import java.io.DataOutputStream;
 import java.io.IOException;
 import java.net.Socket;
+import java.net.SocketTimeoutException;
 
 public class ClientHandler {
     private Server server;
@@ -15,6 +16,8 @@ public class ClientHandler {
     private DataOutputStream out;
     private String nickname;
     private String login;
+
+    private final static int SOCKET_TIMEOUT = 120000;
 
     public ClientHandler(Server server, Socket socket) {
 
@@ -27,6 +30,7 @@ public class ClientHandler {
             new Thread(() -> {
                 try {
                     // цикл аутентификации
+                    this.socket.setSoTimeout(SOCKET_TIMEOUT);
                     while (true) {
                         String str = in.readUTF();
 
@@ -67,6 +71,7 @@ public class ClientHandler {
                         }
                     }
                     //цикл работы
+                    this.socket.setSoTimeout(0);
                     while (true) {
                         String str = in.readUTF();
                         if (str.isEmpty()) {
@@ -88,6 +93,12 @@ public class ClientHandler {
                         } else {
                             server.broadcastMsg(this, str);
                         }
+                    }
+                } catch (SocketTimeoutException e) {
+                    try {
+                        out.writeUTF(Command.END);
+                    } catch (IOException ioException) {
+                        ioException.printStackTrace();
                     }
                 } catch (RuntimeException e) {
                     System.out.println(e.getMessage());

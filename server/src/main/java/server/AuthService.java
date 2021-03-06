54a0cde00b3e8d375107b59d2525a79be1966973
Pavel A. Khanin <pkhanyn@gmail.com>
2021-03-06 18:17:09 +0300

    auth serv

--- server/src/main/java/server/AuthService.java
+++ server/src/main/java/server/AuthService.java
@@ -1,6 +1,6 @@
 package server;
 
-public interface AuthService {
+public interface AuthService  {
     String getNicknameByLoginAndPassword(String login, String password);
     boolean registration(String login, String password, String nickname);
 }
